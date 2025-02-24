733e71d31d98d317ac00120883812dca4bdc5d6f
The Blind SysAdmin <aces4all00@users.noreply.github.com>
2025-02-24 14:42:10 -0800

    Add comment based help to functions

--- Code/BlindSysAdmin.Helper/Public/Start-WinRmCim.ps1
+++ Code/BlindSysAdmin.Helper/Public/Start-WinRmCim.ps1
@@ -1,6 +1,47 @@
 #Requires -version 7.0
 
 function Start-WinRmCim {
+<#
+    .SYNOPSIS
+    Start WinRM service using CIM method
+
+    .DESCRIPTION
+    Attempts to start WinRM service of remote Windows computer using
+    Cim
+
+    .PARAMETER Computer
+    Array of one or more computer names (string) to start WinRM service on
+
+    .INPUTS
+    System.String[]   Computer
+
+    .OUTPUTS
+    System.PSObject   PSCustomObject that reports the results of the operation
+
+    .EXAMPLE
+    PS> Start-WinRmCim -Computer 'SomeComputer01'
+    Computer   : SomeComputer01
+    IsOnline   : TRUE
+    Service    : WinRM
+    Started    : Started
+    ReportDate : 2/24/2025 2:03:16 PM
+
+        Start WinRM Service on Computer 'SomeComputer01'
+
+    .LINK
+    Online version: https://aces4all00.github.com/
+
+    .LINK
+    Online license: https://github.com/aces4all00/BlindSysAdmin.Helper/blob/main/LICENSE
+
+    .LINK
+    Online repository: https://github.com/aces4all00/BlindSysAdmin.Helper
+
+    .NOTES
+    Author: Aces4All00, The Blind SysAdmin
+
+    Date:   2025-02-24
+#>
     [CmdletBinding()]
     param (
         [Parameter(
